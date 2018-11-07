854205afea39c3071958f80f5acfc8f40947a7cf
Shah Zaib <shah743@users.noreply.github.com>
2018-11-07 11:38:32 +0500

    Update tasks.rb

--- lib/mina/unicorn/tasks.rb
+++ lib/mina/unicorn/tasks.rb
@@ -13,22 +13,22 @@ namespace :unicorn do
   set :bundle_gemfile,    -> { "#{fetch(:current_path)}/Gemfile" }
 
   desc "Start Unicorn master process"
-  task start: :remote_environment do
+  task :start do
     command start_unicorn
   end
 
   desc "Stop Unicorn"
-  task stop: :remote_environment do
+  task :stop do
     command kill_unicorn("QUIT")
   end
 
   desc "Immediately shutdown Unicorn"
-  task shutdown: :remote_environment do
+  task :shutdown do
     command kill_unicorn("TERM")
   end
 
   desc "Restart unicorn service"
-  task restart: :remote_environment do
+  task :restart do
     command restart_unicorn
   end
 end
